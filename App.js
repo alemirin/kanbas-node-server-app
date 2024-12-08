c25a3d2b90b4aa2f95c3726892ed241447cf3f7c
alemirin <138611402+alemirin@users.noreply.github.com>
2024-12-08 08:02:47 -0400

    added allowed origin

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   "http://localhost:3000", // Local frontend during development
   "https://6742d2517369db00081d70bc--dapper-tiramisu-b9829b.netlify.app", // Netlify deployed frontend A5
   "https://67558632ef40580008ed88c4--dapper-tiramisu-b9829b.netlify.app", // A6
+  "https://dapper-tiramisu-b9829b.netlify.app",
 ];
 
 const CONNECTION_STRING =
